099157decd68bf6d81f67512eccbff4721070a32
Jinok <joseo46@hotmail.com>
2021-11-04 09:23:41 +1100

    add util.js

---
+++ src/utils/util.js
@@ -0,0 +1,4 @@
+export const addtion = (num1, num2) => num1 + num2;
+export const subtraction = (num1, num2) => num1 - num2;
+export const multiplication = (num1, num2) => num1 * num2;
+export const division = (num1, num2) => num1 / num2;
