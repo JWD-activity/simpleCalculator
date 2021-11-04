1d904d646c6b76131ee38b228b507c8dc2dadc21
Jinok <joseo46@hotmail.com>
2021-11-04 16:37:53 +1100

    modify symbol

--- src/utils/util.js
+++ src/utils/util.js
@@ -7,12 +7,12 @@ export const subtraction = (num1, num2) => {
 };
 
 export const multiplication = (num1, num2) => {
-  return `${num1} * ${num2} = ${num1 * num2}`;
+  return `${num1} × ${num2} = ${num1 * num2}`;
 };
 
 export const division = (num1, num2) => {
-  if (num2 === 0) return `${num1} / ${num2} = \u221e`;
-  return `${num1} / ${num2} = ${num1 / num2}`;
+  if (num2 === 0) return `${num1} ÷ ${num2} = \u221e`;
+  return `${num1} ÷ ${num2} = ${+(num1 / num2).toFixed(3)}`;
 };
 
 export const checkValid = (inputs) => {
