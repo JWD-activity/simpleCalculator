54e57bdf949f7d5cb5b06180d6f4986a46ed5936
Jinok <joseo46@hotmail.com>
2021-11-07 15:50:24 +1100

    modify util.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,65 +1,82 @@
 import { Component } from 'react';
-import './App.css';
-import Card from './components/Card';
-import Error from './components/Error';
-import Footer from './components/Footer';
-import Form from './components/Form';
-import Header from './components/Header';
-import Results from './components/Results';
-import {
-  checkValid,
-  addtion,
-  subtraction,
-  multiplication,
-  division,
-} from './utils/util';
+import Error from './components/Error/Error';
+import Card from './components/Card/Card';
+import Footer from './components/Footer/Footer';
+import Form from './components/Form/Form';
+import Header from './components/Header/Header';
+import Result from './components/Result/Result';
+import Select from './components/Select/Select';
+import { checkValid, checkIsNaN, checkIsNull, calculate } from './utils/util';
 
 class Calculator extends Component {
   state = {
-    firstNum: { input: 0, isValid: true },
-    secondNum: { input: 0, isValid: true },
+    firstNum: { input: null, isValid: true },
+    secondNum: { input: null, isValid: true },
+    operator: null,
   };
 
   changeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value.trim();
-    const validation = +value ? true : false;
 
-    if (!value) this.setState({ [name]: { input: 0, isValid: true } });
-
-    if (validation)
+    // When operator is selected.
+    if (name === 'operator') {
+      this.setState({ operator: value });
+      return;
+    }
+    // When the input is an empty string
+    if (value.length === 0) {
+      this.setState({ [name]: { input: null, isValid: true } });
+      return;
+    }
+    // When the input is 0
+    if (value === '0') {
+      this.setState({ [name]: { input: +value, isValid: true } });
+      return;
+    }
+    // When the input is not the empty string and 0
+    if (value.length !== 0 && value !== '0') {
+      const validation = +value ? true : false;
       this.setState({ [name]: { input: +value, isValid: validation } });
-    else this.setState({ [name]: { input: 0, isValid: validation } });
+      return;
+    }
   };
 
+  // Check validation
   checkValidation = () => {
     const { firstNum, secondNum } = this.state;
     return checkValid([firstNum, secondNum]);
   };
 
+  // Display result
   displayResults = () => {
     const num1 = this.state.firstNum.input;
     const num2 = this.state.secondNum.input;
-    return (
-      <>
-        <p>{addtion(num1, num2)}</p>
-        <p>{subtraction(num1, num2)}</p>
-        <p>{multiplication(num1, num2)}</p>
-        <p>{division(num1, num2)}</p>
-      </>
-    );
+    const operator = this.state.operator;
+
+    if (
+      checkIsNull([num1, num2]) &&
+      checkIsNaN([num1, num2]) &&
+      operator !== null
+    ) {
+      return <p>{calculate(num1, num2, operator)}</p>;
+    } else return <p>PLEASE, ENTER SOMETHING TO BEGIN.</p>;
   };
 
   render() {
+    const first = this.state.firstNum.isValid;
+    const second = this.state.secondNum.isValid;
+
     return (
       <div className='container'>
         <Header />
         <main>
           <Card>
-            <Form changeHandler={this.changeHandler}>
+            <Form changeHandler={this.changeHandler} inputs={[first, second]}>
+              <Select changeHandler={this.changeHandler} />
               {!this.checkValidation() && <Error />}
             </Form>
-            <Results results={this.displayResults} />
+            <Result results={this.displayResults} />
           </Card>
         </main>
         <Footer author='Jinok' />
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,13 +1,9 @@
-// Escaped Unicode
-// ∞ \u221e
-// ² \u00B2
-// √ \u221a
-// × \u00d7
-// ÷ \u00f7
-// + \u002b
-// - \u2212
-// = \u003d
-// ^ \0005e
+// Problem - How to display math symbols.
+// solution 1 - using HTML symbol codes, entities, ASCII characters or Unicode.
+//              Reference https://www.toptal.com/designers/htmlarrows/math/
+//                        https://flaviocopes.com/javascript-unicode/
+//                        https://dmitripavlutin.com/what-every-javascript-developer-should-know-about-unicode/#1-the-idea-behind-unicode
+// solution 2 - using npm packages (e.g react-mathjax, react-katex)
 
 export const calculate = (num1, num2, operator) => {
   switch (operator) {
@@ -19,10 +15,15 @@ export const calculate = (num1, num2, operator) => {
       return `${num1} × ${num2} = ${+(num1 * num2)}`;
     case '÷':
       return num2 === 0
-        ? `${num1} ÷ ${num2} = ∞`
-        : `${num1} ÷ ${num2} = ${+(num1 / num2)}`;
+        ? `${num1} \u{000F7} ${num2} = \u{0221E}`
+        : `${num1} \u{000F7} ${num2} = ${+(num1 / num2)}`;
     case '^':
-      return `${num1} ^ ${num2} = ${Math.pow(num1, num2)}`;
+      return (
+        <>
+          {num1}
+          <sup>{num2}</sup> = {Math.pow(num1, num2)}
+        </>
+      );
     default:
       return;
   }
